c3f5c33e4b329a8e4d60cdc4a8f33e591bd4a233
Rishabh Dixit <rdrishabh38@gmail.com>
2025-02-25 01:08:25 +0530

    fixed model imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,5 @@
 # app/models/__init__.py
-from .base import Base
+
 from .user import User
 from .ride import Ride
 from .driver import Driver
--- app/service/user.py
+++ app/service/user.py
@@ -3,7 +3,7 @@ from sqlalchemy.future import select
 from fastapi import HTTPException
 from passlib.context import CryptContext
 
-from models.user import User
+from models import User
 from schemas.user import UserCreate
 
 # Setup for password hashing
