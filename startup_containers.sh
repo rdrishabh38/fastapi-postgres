585b65d774a53b09d7ed58c398aed6b693e23f14
Rishabh Dixit <rdrishabh38@gmail.com>
2025-02-26 11:02:55 +0530

    fixed startup script

--- startup_containers.sh
+++ startup_containers.sh
@@ -2,13 +2,13 @@
 set -e
 
 echo "Starting Fluentd container..."
-docker-compose up -d fluentd
+docker compose up -d fluentd
 
 echo "Waiting 5 seconds for Fluentd to become healthy..."
 sleep 5
 
 echo "Starting Elasticsearch container..."
-docker-compose up -d elasticsearch
+docker compose up -d elasticsearch
 
 echo "Waiting for Elasticsearch to become healthy..."
 # Loop until Elasticsearch responds (adjust the URL/timeout as needed)
@@ -18,10 +18,10 @@ while ! curl -s http://localhost:9200 >/dev/null; do
 done
 
 echo "Starting Kibana container..."
-docker-compose up -d kibana
+docker compose up -d kibana
 
 echo "Waiting 5 seconds for Kibana..."
 sleep 5
 
 echo "Starting DB and App containers..."
-docker-compose up -d db app
+docker compose up -d db app
